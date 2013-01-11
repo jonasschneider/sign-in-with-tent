a12d71559d151fa3efd0026218b23e347dc72441
Jonas Schneider <mail@jonasschneider.com>
2013-01-11 19:20:51 +0100

    don't authenticate when the user has no entity set

--- chrome-ext/authenticate.js
+++ chrome-ext/authenticate.js
@@ -4,13 +4,21 @@ window.navigator.startActivity = window.navigator.startActivity || window.naviga
 window.intent = window.intent || window.webkitIntent;
 
 okBtn = document.getElementById("ok")
+userEntity = null
+
+chrome.storage.sync.get('tentEntity', function(data) {
+  if(data && typeof data.tentEntity == 'string')
+    userEntity = data.tentEntity
+  else {
+    document.getElementById("alert-notset").classList.remove("hide")
+    document.getElementById("message").classList.add("hide")
+    document.getElementById("ok").classList.add("hide")
+  }
+})
 
 function accept() {
-  chrome.storage.sync.get('tentEntity', function(data) {
-    window.intent.postResult(data.tentEntity)
-  })
+  window.intent.postResult(userEntity)
 }
-
 document.getElementById("cancel").addEventListener("click", function() {
   window.intent.postFailure("user_cancelled")
 })
