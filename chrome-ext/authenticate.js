c7259e952c0a115d4a39eb19ea909cadd11bc3b4
Jonas Schneider <mail@jonasschneider.com>
2013-01-11 19:27:42 +0100

    allow entering the entity manually

--- chrome-ext/authenticate.js
+++ chrome-ext/authenticate.js
@@ -5,19 +5,27 @@ window.intent = window.intent || window.webkitIntent;
 
 okBtn = document.getElementById("ok")
 userEntity = null
+entering = false
 
 chrome.storage.sync.get('tentEntity', function(data) {
   if(data && typeof data.tentEntity == 'string')
     userEntity = data.tentEntity
   else {
+    entering = true
     document.getElementById("alert-notset").classList.remove("hide")
     document.getElementById("message").classList.add("hide")
-    document.getElementById("ok").classList.add("hide")
   }
 })
 
 function accept() {
-  window.intent.postResult(userEntity)
+  if(entering) {
+    userEntity = document.getElementById("entity-input").value
+    chrome.storage.sync.set({'tentEntity': userEntity}, function() {
+      window.intent.postResult(userEntity)
+    })
+  } else {
+    window.intent.postResult(userEntity)
+  }
 }
 document.getElementById("cancel").addEventListener("click", function() {
   window.intent.postFailure("user_cancelled")
